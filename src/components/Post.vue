86c01bdaa82a0eaeb674208a79be63fd79bd25cc
liron hayne <lironhayne555@gmail.com>
2024-07-07 19:58:02 +0300

    post css

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="post mx-sm px-sm">
     <div class="post-header column my-xs">
-      <div class="user-info row justify-between align-center">
-        {{ post.user }}
-        {{ post.createdAt }}
+      <div class="user-info column justify-between">
+       <div class="user-text my-xs"> {{ post.user }} </div>
+       <div class="created-text my-xs"> {{ post.createdAt }}</div>
       </div>
     </div>
     <div class="post-body">
-      <div class="post-text">{{ post.text }}</div>
-      <div class="post-image" v-if="post.image">
+      <div class="post-text my-xs">{{ post.text }}</div>
+      <div class="post-image my-sm" v-if="post.image">
         <img :src="post.image" alt="Post image" />
       </div>
     </div>
-    <div class="post-footer colume">
+    <div class="post-footer row ">
       <div class="likes mx-sm">
         <span>{{ post.likes.length }} Likes</span>
       </div>
@@ -35,23 +35,35 @@ const props = defineProps<PostProps>();
 
 <style lang="scss" scoped>
 .post {
-  background-color: #ccddff;
-  border-radius: 10px;
+  background-color: #cecfd34f;
+  border-radius: 20px;
+  padding-inline: 30px;
+  padding-top: 20px;
 }
-.post-text {
+.user-info{
   color: #555;
+}
+.post-footer{
+margin-bottom: 20px;
 }
 .post-image {
   img {
-    width: 500px;
+    width: 300px;
     max-height: 600px;
     object-fit: cover;
+    border-radius: 5px;
+  
   }
   border-radius: 10px;
 }
 .likes,
 .comments {
-  cursor: pointer;
+ 
   color: #007bff;
+}
+.likes:hover,
+.comments:hover {
+  text-decoration: underline;
+ cursor: pointer;
 }
 </style>
